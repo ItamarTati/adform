c206cd8a9d5c998ea49f52daf518572a51c4d3fb
Itamar Tati <itamar.tati1994@gmail.com>
2021-03-11 22:58:57 +0000

    added more files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchName from './SearchName'
+import FilterPopulation from './FilterPopulation'
+import Cards from './Cards'
+
+
+
+export default function App() {
+    return(
+    <div>
+        <SearchName />
+        <FilterPopulation />
+        <Cards />
+    </div> 
+    )
+}
+
---
+++ src/FilterPopulation.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FilterPopulation() {
+    return <div></div>
+}
